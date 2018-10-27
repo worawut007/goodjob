cea56d50074c29adf4ed8c0b0bc16d006a10ddf3
Bigbox <thurakit.big@gmail.com>
2018-10-27 18:03:52 +0700

    detail-work component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailWorkComponent } from './detail-work/detail-work.component';
 
 const routes: Routes = [
     {
       path: '',
       loadChildren: './dashboard/dashboard.module#DashboardModule',
+    },
+    {
+      path: 'job-description',
+      component: DetailWorkComponent
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { DetailWorkComponent } from './detail-work/detail-work.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { SidebarComponent } from './component/sidebar/sidebar.component';
     AppComponent,
     HeaderComponent,
     SidebarComponent,
+    DetailWorkComponent,
   ],
   imports: [
     BrowserModule,
