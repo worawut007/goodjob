9fdf860ae0e31c346162dbf52b94108f62fa4677
worawut <boyjam1@hotmail.com>
2018-10-27 17:42:48 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
-import { SubheaderComponent } from './component/subheader/subheader.component';
 import { MailInboxComponent } from './mail/mail-inbox/mail-inbox.component';
 import { ProfileComponent } from './profile/profile/profile.component';
 
@@ -15,7 +14,6 @@ import { ProfileComponent } from './profile/profile/profile.component';
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    SubheaderComponent,
     MailInboxComponent,
     ProfileComponent,
   ],
