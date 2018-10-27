d5dae67a11ac813d1cd91a5b28ca960899708a97
worawut <boyjam1@hotmail.com>
2018-10-27 14:58:49 +0700

    วางโครงสร้าง

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
